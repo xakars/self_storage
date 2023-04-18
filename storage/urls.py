e0156ffb6cedd749b5dedb1819fd7e1a87368d97
ars <xakars02@gmail.com>
2023-04-18 22:01:36 +0600

    add path to pages

--- storage/urls.py
+++ storage/urls.py
@@ -8,6 +8,9 @@ app_name = "storage"
 
 urlpatterns = [
     path('', lambda request: redirect('storage:start_page')),
-    path('index/', views.view_index, name='start_page')
-
+    path('index/', views.view_index, name='start_page'),
+    path('boxes/', views.view_boxes, name='view_boxes'),
+    path('faq/', views.view_faq, name='view_faq'),
+    path('my_rent/', views.view_my_rent, name='view_my_rent'),
+    path('my_rent_empty/', views.view_my_rent_empty, name='view_my_rent_empty'),
 ]
--- storage/views.py
+++ storage/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def view_index(request):
     return render(request, template_name="index.html", context={})
+
+
+def view_boxes(request):
+    return render(request, template_name="boxes.html", context={})
+
+
+def view_faq(request):
+    return render(request, template_name="faq.html", context={})
+
+
+def view_my_rent(request):
+    return render(request, template_name="my-rent.html", context={})
+
+
+def view_my_rent_empty(request):
+    return render(request, template_name="my-rent-empty.html", context={})
