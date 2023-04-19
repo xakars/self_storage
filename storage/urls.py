d8404db5aa80524cf5c9180d5816967ea11751eb
Olga <o.panova27@gmail.com>
2023-04-19 19:22:39 +0300

    Add base template

--- storage/urls.py
+++ storage/urls.py
@@ -7,8 +7,8 @@ app_name = "storage"
 
 
 urlpatterns = [
-    path('', lambda request: redirect('storage:start_page')),
-    path('index/', views.view_index, name='start_page'),
+    # path('', lambda request: redirect('storage:start_page')),
+    path('', views.view_index, name='start_page'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout, name="logout"),
